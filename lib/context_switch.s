72a20229f065d912c4524426edee139e0f701753
Joseph Ryan <josephryan3.14@gmail.com>
2020-03-09 19:39:10 -0500

    Pull out context switch assembly

---
+++ lib/context_switch.s
@@ -0,0 +1,22 @@
+.cpu cortex-m4
+.fpu fpv4-sp-d16
+.syntax unified
+.thumb
+.text
+
+.extern current_thread
+.global pendsv_handler
+
+.thumb_func
+pendsv_handler:
+    CPSID I
+    PUSH {R4 - R11}
+    LDR  R0, =current_thread    // R0 = &current_thread
+    LDR  R1, [R0]               // R1 = current_thread
+    STR  SP, [R1]               // SP = *current_thread aka current_thread->sp
+    LDR  R1, [R1,#4]            // R1 = current_thread->next_tcb;
+    STR  R1, [R0]               // current_thread = current_thread->next_tcb;
+    LDR  SP, [R1]               // SP = current_thread->sp
+    POP  {R4 - R11}
+    CPSIE I
+    BX LR
