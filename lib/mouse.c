984bbed9232028073d2d3ae4e65513f9496694a3
Vivek Malneedi <vivekmalneedi@gmail.com>
2020-05-06 11:44:22 -0400

    mouse semaphore

--- lib/mouse.c
+++ lib/mouse.c
@@ -8,8 +8,6 @@
 #include "usblib/device/usbdhidmouse.h"
 #include "usblib/usblib.h"
 
-volatile bool ready = false;
-
 const uint8_t LangDescriptor[] = {4, USB_DTYPE_STRING,
                                   USBShort(USB_LANG_EN_US)};
 const uint8_t ManufacturerString[] = {
@@ -37,32 +35,32 @@ const uint8_t* const StringDescriptors[] = {
     LangDescriptor,     ManufacturerString, ProductString,
     SerialNumberString, HIDInterfaceString, ConfigString};
 
+static Sema4 mouse_ready;
 uint32_t mouse_handler(void* pvCBData, uint32_t ui32Event, uint32_t ui32MsgData,
                        void* pvMsgData) {
     switch (ui32Event) {
     case USB_EVENT_CONNECTED: {
         printf("\nHost Connected...\r\n");
-        ready = true;
+        OS_Signal(&mouse_ready);
         break;
     }
     case USB_EVENT_DISCONNECTED: {
         printf("\nHost Disconnected...\r\n");
-        ready = false;
         break;
     }
     case USB_EVENT_TX_COMPLETE: {
         // printf("TX complete\n\r");
-        ready = true;
+        OS_Signal(&mouse_ready);
         break;
     }
     case USB_EVENT_SUSPEND: {
+        OS_Signal(&mouse_ready);
         printf("\nBus Suspended\n\r");
-        ready = false;
         break;
     }
     case USB_EVENT_RESUME: {
+        OS_Signal(&mouse_ready);
         printf("\nBus Resume\n\r");
-        ready = true;
         break;
     }
         // We ignore all other events.
@@ -96,8 +94,7 @@ typedef enum { LEFT = 1, RIGHT, MIDDLE } MouseButton;
 static bool held_down[3] = {false, false, false};
 void mouse_toggle(MouseButton button) {
     --button;
-    while (!ready) {}
-    ready = false;
+    OS_Wait(&mouse_ready);
     USBDHIDMouseStateChange(&mouse_dev, 0, 0,
                             held_down[button] ? 0 : 1 << button);
     held_down[button] = !held_down[button];
@@ -108,8 +105,7 @@ void mouse_click(MouseButton button) {
 }
 
 void mouse_move(int8_t x, int8_t y) {
-    while (!ready) {}
-    ready = false;
+    OS_Wait(&mouse_ready);
     USBDHIDMouseStateChange(&mouse_dev, x, y, 0);
 }
 
@@ -181,13 +177,14 @@ bool mouse_cmd(char c) {
 }
 
 void mouse_init(void) {
+    OS_InitSemaphore(&mouse_ready, -1);
     ROM_GPIOPinTypeUSBAnalog(GPIO_PORTD_BASE, GPIO_PIN_4 | GPIO_PIN_5);
     USBStackModeSet(0, eUSBModeForceDevice, 0);
 
     if (!USBDHIDMouseInit(0, &mouse_dev)) {
         printf("error: hidmouseinit\n\r");
     }
-    while (!ready) {}
+    OS_Wait(&mouse_ready);
     OS_AddThread(mouse_continuous, "Continuous mouse movement", 512, 1);
     OS_Sleep(ms(1500));
 }
